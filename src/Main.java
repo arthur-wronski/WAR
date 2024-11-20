230a9b561751367b25bae2658eda2177f62c5da0
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-11-20 11:48:19 +0000

    [FEAT] basic main class for testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Deck deck = new Deck();
+        deck.generateStartingDeck();
+        System.out.println("Deck Size: " + deck.getDeckSize());
+        System.out.println("First Card: " + deck.cards.getFirst());
+        System.out.println("First Card value: " + deck.cards.getFirst().getRankValue());
     }
 }
